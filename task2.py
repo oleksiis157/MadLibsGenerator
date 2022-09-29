f0c3c6f7be80477bfb8f2023fe12c40fb2c8ee04
Oleksii <oleksiis157@gmail.com>
2022-09-29 22:07:16 +0200

    task2.py

---
+++ task2.py
@@ -0,0 +1,26 @@
+#Write a simple function that calculates price of the door.
+  #Function should take 2 parameters: height and width, and return price of the door.
+ 
+  #Price calculation:
+ #price = square * 1.57
+
+
+#price = float(1.57)
+#classical = "classical door"
+#door = input("""Input your door:
+#(classical, triangle, square)""")
+
+#def classicla_door ():
+ #   height = float(input())
+  #  width = float(input())
+
+
+
+#a = int(input("Input 1st side: "))
+#b = int(input("Input 2nd side: "))
+#c = int(input("Input 3st side: "))
+#p_triangle = a + b + c
+
+
+#print((height*width)*price)
+
