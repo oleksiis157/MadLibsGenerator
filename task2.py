14c1f07015252260306d7104fd0d41af41ba06eb
Oleksii <oleksiis157@gmail.com>
2022-10-01 13:31:58 +0200

    task2.py

--- task2.py
+++ task2.py
@@ -2,25 +2,37 @@
   #Function should take 2 parameters: height and width, and return price of the door.
  
   #Price calculation:
- #price = square * 1.57
+ 
 
 
-#price = float(1.57)
+
 #classical = "classical door"
 #door = input("""Input your door:
 #(classical, triangle, square)""")
 
-#def classicla_door ():
- #   height = float(input())
-  #  width = float(input())
-
-
-
-#a = int(input("Input 1st side: "))
-#b = int(input("Input 2nd side: "))
-#c = int(input("Input 3st side: "))
-#p_triangle = a + b + c
-
-
-#print((height*width)*price)
+#def my_doore(type):
+price = float(1.57)
+type_doore = input("Input type of door (classical, triangle, circle): ")
+if (type_doore == "classical"):     
+      height = float(input("Input the height of yor door: "))
+      width = float(input("Input the width of yor door: "))
+      conc_classical = (height*width)*price
+      print("The sum of your classical door = ", conc_classical,"$")
+elif (type_doore == "triangle"):
+      a = float(input("Input 1st side: "))
+      b = float(input("Input 2nd side: "))
+      c = float(input("Input 3st side: "))
+      p_triangle = (a+b+c)/2
+      s_triangle = p_triangle*(p_triangle-a)*(p_triangle-b)*(p_triangle-c)
+      print(s_triangle)
+      print("The sum of your triangle door = ", s_triangle*price,"$")
+elif (type_doore == "circle"):
+      p = 3.14
+      d_circle = float(input("""Input diametr of circle 
+      (just a number): """))
+      r_circle = d_circle/2
+      print("The square of circle = ", p*(r_circle**2))
+      print("The sum of circle door = ", (p*(r_circle**2))*price,"$")  
+else:
+      print("You don't need a door...")  
 
